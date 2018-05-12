1d799b6a7ec2e5ef635bf4d214f1b5d29c12a5e1
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-05-12 19:31:29 +0700

    Update Fizz Buzz

--- FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz.py
@@ -2,6 +2,11 @@ def checkio(num_: int) -> str:
     return "Fizz Buzz" if num_ % 15 == 0 else 'Fizz' if num_ % 3 == 0 else 'Buzz' if num_ % 5 == 0 else str(num_)
 
 
+def checkio2(n):
+    # https://py.checkio.org/mission/fizz-buzz/publications/panaro32/python-3/first/
+    return 'Fizz'*(not n % 3)+' '*(not n % 15)+'Buzz'*(not n % 5) or str(n)
+
+
 if __name__ == '__main__':
     assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
     assert checkio(6) == "Fizz", "6 is divisible by 3"
