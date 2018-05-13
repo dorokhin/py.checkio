2380456f55b3aacaa08621956a71ef917a64643a
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-05-13 18:16:10 +0700

    Update Even The Last

--- Elementary/EvenTheLast/EvenTheLast.py
+++ Elementary/EvenTheLast/EvenTheLast.py
@@ -2,6 +2,10 @@ def checkio(array_):
     return 0 if len(array_) == 0 else sum(array_[::2]) * array_[-1]
 
 
+def checkio2(array_):
+    return sum(array_[::2])*array_[-1] if array_ else 0
+
+
 if __name__ == '__main__':
     assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
     assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
