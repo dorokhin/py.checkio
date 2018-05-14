adb64df0d73146169cbc0d3fcdaddf220c288dc7
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-05-14 15:17:28 +0700

    Update Long Repeat

--- home/LongRepeat/LongRepeat.py
+++ home/LongRepeat/LongRepeat.py
@@ -3,6 +3,13 @@ def long_repeat(line):
                 if any([True for char in line if line.find(char * i) != -1])]) if line != '' else 0
 
 
+def long_repeat2(line):
+    return len(line) and 1+long_repeat(''.join(u for u,v in zip(line,line[1:]) if u==v))
+
+
+long_repeat3 = lambda l: len(l)and max(map(len, dict(__import__('re').findall(r'((.)\2*)', l))))
+
+
 if __name__ == '__main__':
     assert long_repeat('sdsffffse') == 4, "First"
     assert long_repeat('ddvvrwwwrggg') == 3, "Second"
