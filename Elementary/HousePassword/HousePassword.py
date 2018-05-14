0d324bfa17c4c1df8bb4be76b3239b38bdfc1b0d
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-05-14 15:17:40 +0700

    Update House Password

--- Elementary/HousePassword/HousePassword.py
+++ Elementary/HousePassword/HousePassword.py
@@ -2,6 +2,8 @@ def checkio(d: str) -> bool:
     return True if len(d) >= 10 and any(c.isdigit() for c in d) and any(c.isupper() for c in d) and any(c.islower() for c in d)else False
 
 
+checkio2 = lambda s: not(len(s) < 10 or s.isdigit() or s.isalpha() or s.islower() or s.isupper())
+
 if __name__ == '__main__':
     assert checkio('A1213pokl') == False, "1st example"
     assert checkio('bAse730onE4') == True, "2nd example"
